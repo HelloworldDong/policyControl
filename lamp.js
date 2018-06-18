38fe96552d93e3bb7db6de2d81a4903a27a1fa1e
dongliang <534096734@qq.com>
2018-06-18 23:52:04 +0800

    test script

--- lamp.js
+++ lamp.js
@@ -3,12 +3,13 @@ var client = mqtt.connect('mqtt://localhost:8000');
 
 var device = {id:1,devID:1,level:100,running:1,wind:30,temp:26,air:11,time:"2018-04-28 19:39:51"};
 
+var IDlist = [1,10,20,18]
 client.on('connect', function () {
   client.publish('test',JSON.stringify(device),{qos:1,retain:'ture'});
   setInterval(function(){
     var lamp ={};
     lamp.id= Math.floor(Math.random()*100);
-    lamp.devID= Math.floor(Math.random()*100);
+    lamp.devID=  IDlist[Math.round(Math.random()*(IDlist.length-1))];
     lamp.level = Math.floor(Math.random()*100);
     lamp.running= Math.floor(Math.random());
     lamp.wind= Math.floor(Math.random()*50);
